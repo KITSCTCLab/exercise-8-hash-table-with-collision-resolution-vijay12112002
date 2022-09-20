eed80c5c84f28186bdfe9143858c1bf43101e002
vijay12112002 <110010946+vijay12112002@users.noreply.github.com>
2022-09-20 11:38:24 +0530

    Initial commit

--- Main.py
+++ Main.py
@@ -1,13 +1,21 @@
 import re
 
 def display_hash(hashtable) -> None:
-	for i in range
-	
-
+	# Write your code here
+	for keyvalue in range(len(hashtable)):
+		content=hashtable[keyvalue]
+		print(keyvalue,end=" ")
+		for element in content:
+			print("-->",element,end=" ")
+		print()
+			
 def Hashing(keyvalue) -> int:
 	return keyvalue % len(HashTable)
 
 def insert(Hashtable, keyvalue, value) -> None:
+	# Write your code here
+	Hashtable[Hashing(keyvalue)].append(value)
+	
 	
 
 # Do not edit the following code
